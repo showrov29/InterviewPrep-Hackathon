f7abc517e45b4bc4a52f0fa4297ff0ca07f24f62
Dhruba Chakraborty <dhruba.chakraborty.2000@gmail.com>
2025-01-17 01:25:49 +0600

    refactor: comment out unused camera controls and adjust scene position for better alignment

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ scene = new THREE.Scene();
 // Create a camera
 camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 // camera.position.set(0.33,5,10.5)
-camera.position.set(0,0,1)
+// camera.position.set(0,0,1)
 // Create a renderer
 renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -45,10 +45,10 @@ scene.add(ambientLight);
 const light = new THREE.DirectionalLight(0xffffff, 3);
 light.position.set(1, 1, 1).normalize();
 scene.add(light);
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.dampingFactor = 0.25;
-controls.enableZoom = true;
+// const controls = new OrbitControls(camera, renderer.domElement);
+// controls.enableDamping = true;
+// controls.dampingFactor = 0.25;
+// controls.enableZoom = true;
 // Load the GLB model
 const loader = new GLTFLoader();
 loader.load(
@@ -70,6 +70,7 @@ loader.load('sittingHR.glb', function (gltf) {
   dictionanry = avatar.children[0].getObjectByName('Wolf3D_Avatar').morphTargetDictionary;
   scene.add(avatar);
   avatar.position.set(0, -1.65, -0.5);
+  scene.position.set(0, 0.4, -0.9)
   // avatar.scale.set(2,2,2)
 });
 

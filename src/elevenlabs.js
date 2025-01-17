d668188f9c868b97ac7d6c4ad37d88d7f5f29209
Dhruba Chakraborty <dhruba.chakraborty.2000@gmail.com>
2025-01-17 18:16:34 +0600

    feat: add end button functionality and update button visibility handling

--- src/elevenlabs.js
+++ src/elevenlabs.js
@@ -121,7 +121,7 @@ async function getAudioAndCharsFromElevenLabs(text ) {
             stability: 0.2,
             similarity_boost: 0.5,
           },
-          xi_api_key: "sk_6e33f5b66e721da18f8773393324355639c3d0ee0255556b",
+          xi_api_key: "sk_8898c7f068cbd5d2a98bdcb6977279f90be22837162667c4",
         };
         elevenLabsSocket.send(JSON.stringify(bosMessage));
         const textMessage = {
